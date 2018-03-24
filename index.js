1611f1c3e58f91b39e17086f62eb968efdb3644a
Audrey Schmook <audrey.schmook@gmail.com>
2018-03-24 11:58:38 -0500

    new commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var $ = require("jquery");
+var _ = require("lodash");
+
+var list = [ "john", "paul", "george", "ringo"];
+
+function message(){
+	console.log("This is a message");
+	alert("This is a message")
+}
+
+$(document).ready(function(){
+	$("#nameList").append("Our list is going to go here!");
+});
+
+window.message = message;
